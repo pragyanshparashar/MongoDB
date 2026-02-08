fa355680494e1241d41b1b66246594902e5ebecd
Pragyansh Parashar <pragyanshparashar122@gmail.com>
2026-02-08 23:02:06 +0530

    Implement CRUD operations using Mongoose

--- app.js
+++ app.js
@@ -8,11 +8,21 @@ app.get('/',function(req,res){
 
 app.get('/create',async function(req,res){
  const createdUser = await userModel.create({
-    name:'pragyansh',
-    username:'pragyanshK',
-    email: 'pragyansh@example.com'
+    name:'tom',
+    username:'tom cruise',
+    email: 'tom@example.com'
  })
  res.send(createdUser);
+})
+
+app.get('/update',async function(req,res){
+    const updatedUser = await userModel.findOneAndUpdate({username:'pragyanshK'},{name:'pragyanshparashara'},{new:true})
+    res.send(updatedUser)
+})
+
+app.get('/read', async function(req,res){
+    const readUser = await userModel.find()
+    res.send(readUser);
 })
 
 app.listen(4000,function(){
