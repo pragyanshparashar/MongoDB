aa32a19ff6ab0a499e38d2af5793597196ac0213
Pragyansh Parashar <pragyanshparashar122@gmail.com>
2026-02-10 17:12:28 +0530

    Complete user CRUD app with Express, MongoDB, EJS and clean project structure

--- app.js
+++ app.js
@@ -1,30 +1,55 @@
 const express = require('express');
 const app = express();
-const userModel= require('./usermodel')
+const path = require('path')
+const userModel = require('./model/user');
 
-app.get('/',function(req,res){
-    res.send('hey lets start with mongodb')
-})
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/create',async function(req,res){
- const createdUser = await userModel.create({
-    name:'tom',
-    username:'tom cruise',
-    email: 'tom@example.com'
- })
- res.send(createdUser);
-})
-
-app.get('/update',async function(req,res){
-    const updatedUser = await userModel.findOneAndUpdate({username:'pragyanshK'},{name:'pragyanshparashara'},{new:true})
-    res.send(updatedUser)
+app.get('/', function(req,res){
+  res.render ('index')
 })
 
 app.get('/read', async function(req,res){
-    const readUser = await userModel.find()
-    res.send(readUser);
+    const users = await userModel.find()
+    res.render ('read', {users})
 })
 
-app.listen(4000,function(){
-    console.log('server is running on port 4000')   
+app.post('/create', async function(req,res){
+    const {name,email,image} = req.body;
+    const createdUser = await userModel.create({
+        name,
+        email,
+        image
+    })
+    res.redirect('/read');
+})
+
+app.get('/delete/:id', async function(req,res){
+    const {id} = req.params;
+    await userModel.findOneAndDelete({_id: id});
+    res.redirect('/read');
+})
+
+app.get('/edit/:id', async function(req,res){
+    const {id} = req.params;
+    const user = await userModel.findOne({_id: id});
+    res.render('edit', {user});
+})
+
+app.post('/Update/:id', async function(req,res){
+    const {id} = req.params;
+    const {name,email,image} = req.body;
+    await userModel.findOneAndUpdate({_id: id}, {
+        name,
+        email,
+        image
+    })
+    res.redirect('/read');
+})
+
+app.listen(4000, function(){
+    console.log('Server is running on http://localhost:4000');
 })
